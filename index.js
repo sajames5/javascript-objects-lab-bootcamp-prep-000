2de95ba9b287d1b59efb695c599a5e55b6686725
Stephanie <s.a.james5@gmail.com>
2018-01-19 18:49:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,6 +12,6 @@ function deleteFromObjectByKey(object, key) {
   return newObject
 }
 function destructivelyDeleteFromObjectByKey(object, key) {
-  delete object.key
+  delete object[key]
   return object
 }
